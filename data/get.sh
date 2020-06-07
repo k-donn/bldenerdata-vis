6beefe1877d6040c9d02eed5fb4a0ce699f3011f
k-donn <mitchell@kevinandjudy.com>
2020-06-06 21:36:08 -0400

    Added proper line-by-line JSONL processing

--- data/get.sh
+++ data/get.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
+find . -name "*.json*" -delete
 curl https://opendata.blender.org/snapshots/opendata-latest.zip -o latest.zip
 unzip latest.zip
 find README.txt LICENSE.txt latest.zip -delete
 
-mv *.jsonl raw.json
-# Add first bracket and commas
-sed -i '1s/^/[/g' raw.json
-sed -i 's/$/,/g' raw.json
-# delete last spare line and comma
-sed -i '$d' raw.json
-sed -i '$ s/.$//' raw.json
-# put closing bracket
-echo "]" >> raw.json
-
+mv *.jsonl raw.jsonl
--- parse.js
+++ parse.js
@@ -1,71 +1,97 @@
 let fs = require("fs");
-let raw = require("./data/raw.json");
+let readline = require("readline");
 
 let Database = require("better-sqlite3");
 let db = new Database("./data/blender-opendata.db");
 
-let sceneTimes = raw.flatMap((benchmark) => {
-	let res = [];
-	if (
-		benchmark.schema_version === "v1" ||
-		benchmark.schema_version === "v2"
-	) {
-		let scenes = benchmark.data.scenes;
-		scenes.forEach((scene) => {
-			if (scene.stats.result === "OK") {
-				res.push({
-					device:
-						benchmark.schema_version === "v1"
-							? benchmark.data.device_info.compute_devices[0]
-							: benchmark.data.device_info.compute_devices[0]
-									.name,
-					scene: scene.name,
-					time: scene.stats.total_render_time,
-				});
-			}
-		});
-	} else {
-		let launches = benchmark.data;
-		launches.forEach((run) => {
-			res.push({
-				device: run.device_info.compute_devices[0].name,
-				scene: run.scene.label,
-				time: run.stats.total_render_time,
-			});
-		});
-	}
+let fileStream = fs.createReadStream("./data/raw.jsonl");
 
-	return res;
-});
+async function processLineByLine() {
+	let sceneTimes = [],
+		lineNum = 0;
 
-try {
-	fs.writeFile("./data/parsed.json", JSON.stringify(sceneTimes), () => {
-		console.log("Wrote parsed file.");
+	// The file is a JSON-L file so it should be processed line-by-line
+	let rl = readline.createInterface({
+		input: fileStream,
 	});
-} catch (err) {
-	console.log("Error writing parsed file:", err);
+
+	let startProcess = new Date().getTime();
+	for await (let line of rl) {
+		let benchmark = JSON.parse(line);
+		if (
+			benchmark.schema_version === "v1" ||
+			benchmark.schema_version === "v2"
+		) {
+			let scenes = benchmark.data.scenes;
+			scenes.forEach((scene) => {
+				if (scene.stats.result === "OK") {
+					lineNum++;
+
+					sceneTimes.push({
+						device:
+							benchmark.schema_version === "v1"
+								? benchmark.data.device_info.compute_devices[0]
+								: benchmark.data.device_info.compute_devices[0]
+										.name,
+						scene: scene.name,
+						time: scene.stats.total_render_time,
+					});
+				}
+			});
+		} else {
+			let launches = benchmark.data;
+			launches.forEach((run) => {
+				lineNum++;
+
+				sceneTimes.push({
+					device: run.device_info.compute_devices[0].name,
+					scene: run.scene.label,
+					time: run.stats.total_render_time,
+				});
+			});
+		}
+	}
+	console.log(
+		`Processed \x1b[35m${lineNum}\x1b[0m lines in \x1b[32m${
+			new Date().getTime() - startProcess
+		}\x1b[0mms`
+	);
+	return [lineNum, sceneTimes];
 }
 
-let createTable = db.prepare(
-	`CREATE TABLE IF NOT EXISTS blender (
-		device TEXT,
-		scene TEXT,
-		time REAL
-	)`
-);
-createTable.run();
-let deleteRows = db.prepare(`DELETE FROM blender`);
-deleteRows.run();
-let putRow = db.prepare("INSERT INTO blender VALUES (?,?,?)");
+(async () => {
+	try {
+		// inserting values into the db goes much faster when inside a transaction
+		let createTable = db.prepare(
+			`CREATE TABLE IF NOT EXISTS blender (
+				device TEXT,
+				scene TEXT,
+				time REAL
+			)`
+		);
+		createTable.run();
+		let deleteRows = db.prepare(`DELETE FROM blender`);
+		deleteRows.run();
+		let putRow = db.prepare("INSERT INTO blender VALUES (?,?,?)");
 
-let insert = db.transaction((trials) => {
-	for (let trial of trials) {
-		putRow.run(trial["device"], trial["scene"], trial["time"]);
+		let [sceneTmLen, sceneTimes] = await processLineByLine();
+		fs.writeFile("./data/parsed.json", JSON.stringify(sceneTimes), () => {
+			console.log("Wrote \x1b[33mdata/parsed.json\x1b[0m");
+		});
+		let insert = db.transaction((trials) => {
+			for (let trial of trials) {
+				putRow.run(trial["device"], trial["scene"], trial["time"]);
+			}
+		});
+
+		let startWrite = new Date().getTime();
+		insert(sceneTimes);
+		console.log(
+			`Wrote \x1b[35m${sceneTmLen}\x1b[0m lines to db in \x1b[32m${
+				new Date().getTime() - startWrite
+			}\x1b[0mms`
+		);
+	} catch (err) {
+		console.error(err);
 	}
-});
-
-let start = new Date().getTime();
-insert(sceneTimes);
-console.log(
-	`Wrote ${sceneTimes.length} rows to db in ${new Date().getTime() - start}ms`
-);
+})();
