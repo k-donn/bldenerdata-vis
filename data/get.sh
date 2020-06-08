ec0d6eea30240de4847be8eeaff3749fac65a2c7
k-donn <mitchell@kevinandjudy.com>
2020-06-08 17:22:52 -0400

    Workflow pulls at 07:00 UTC, when data is released

--- data/get.sh
+++ data/get.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+echo "Deleting old json*"
 find . -name "*.json*" -delete
-curl https://opendata.blender.org/snapshots/opendata-latest.zip -o latest.zip
-unzip latest.zip
-find README.txt LICENSE.txt latest.zip -delete
 
+echo "Downloading new raw json*"
+curl https://opendata.blender.org/snapshots/opendata-latest.zip -o latest.zip
+
+echo "Uncompressing"
+unzip latest.zip -x LICENSE.txt README.txt
+
+echo "Tidying up"
+
+find . -name "opendata-*.jsonl"
+echo "->"
+echo "./raw.jsonl"
 mv *.jsonl raw.jsonl
+
+find latest.zip -delete
